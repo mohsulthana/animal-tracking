82d82f573312a6c76ea45fb9e33a6f9e8ce51ac5
Mohammad Sulthan <muhammadsultana@outlook.com>
2024-04-07 01:18:53 +0700

    first commit

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <h2>Search and add a pin</h2>
+    </div>
+    <br>
+    <GmapMap
+      :center="center"
+      :zoom="12"
+      style="width:100%;  height: 400px;"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoogleMap',
+  data() {
+    return {
+      center: { lat: -28.2293, lng: 25.7479 }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
